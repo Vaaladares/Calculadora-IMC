621b01353d4b9149b0996b0b841ee2c7938cf954
Vinicius Valadares Chinelatto <103156091+Vaaladares@users.noreply.github.com>
2023-06-09 19:35:26 -0300

    Atualizando

--- script.js
+++ script.js
@@ -49,3 +49,22 @@ function calcular() {
     }
     resimc.innerHTML = imc
 }
+
+function limpar() {
+    var altura = document.getElementById("altura")
+    var peso = document.getElementById("peso")
+    var resimc = document.getElementById("res-imc")
+
+    altura.value = ''
+    peso.value = ''
+
+    if(altura.value == '' && peso.value == '') {
+        magreza.style.backgroundColor = "white"
+        normal.style.backgroundColor = "white"
+        sobrepeso.style.backgroundColor = "white"
+        obesidade.style.backgroundColor = "white"
+        obesidadeGrave.style.backgroundColor = "white"
+    }
+    
+    resimc.innerHTML = ''
+}
