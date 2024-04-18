3980416ed799f7ed1d740a6a184373e3b5891543
Vinicius Valadares Chinelatto <103156091+Vaaladares@users.noreply.github.com>
2024-04-17 22:13:32 -0300

    atualizando detalhes

--- script.js
+++ script.js
@@ -3,17 +3,17 @@ function calcular() {
     var peso = document.getElementById("peso").value
     var imc = (peso / altura**2).toFixed(2) 
 
-    var tr = document.getElementById("magreza")
-    var tr = document.getElementById("normal")
-    var tr = document.getElementById("sobrepeso")
-    var tr = document.getElementById("obesidade")
-    var tr = document.getElementById("obesidadeGrave")
+    var magreza = document.getElementById("magreza")
+    var normal = document.getElementById("normal")
+    var sobrepeso = document.getElementById("sobrepeso")
+    var obesidade = document.getElementById("obesidade")
+    var obesidadeGrave = document.getElementById("obesidadeGrave")
     
-    var resimc = document.getElementById("res-imc")
+    var resImc = document.getElementById("res-imc")
 
     if(altura == '' || peso == ''){
         window.alert("Erro! Preencha os campos.")
-        resimc.innerHTML = ''
+        resImc.innerHTML = ''
     }
     else if ( imc < 18.5) {
         magreza.style.backgroundColor = "#bbd8ff77"
@@ -21,7 +21,7 @@ function calcular() {
         sobrepeso.style.backgroundColor = "white"
         obesidade.style.backgroundColor = "white"
         obesidadeGrave.style.backgroundColor = "white"
-        resimc.innerHTML = imc
+        resImc.innerHTML = imc
     }
     else if (imc < 24.9) {
         magreza.style.backgroundColor = "white"
@@ -29,7 +29,7 @@ function calcular() {
         sobrepeso.style.backgroundColor = "white"
         obesidade.style.backgroundColor = "white"
         obesidadeGrave.style.backgroundColor = "white"
-        resimc.innerHTML = imc
+        resImc.innerHTML = imc
     }
     else if (imc < 29.9) {
         magreza.style.backgroundColor = "white"
@@ -37,7 +37,7 @@ function calcular() {
         sobrepeso.style.backgroundColor = "#bbd8ff77"
         obesidade.style.backgroundColor = "white"
         obesidadeGrave.style.backgroundColor = "white"
-        resimc.innerHTML = imc
+        resImc.innerHTML = imc
     }
     else if (imc < 39.9) {
         magreza.style.backgroundColor = "white"
@@ -45,7 +45,7 @@ function calcular() {
         sobrepeso.style.backgroundColor = "white"
         obesidade.style.backgroundColor = "#bbd8ff77"
         obesidadeGrave.style.backgroundColor = "white"
-        resimc.innerHTML = imc
+        resImc.innerHTML = imc
     }
     else if (imc > 40.0) {
         magreza.style.backgroundColor = "white"
@@ -53,14 +53,11 @@ function calcular() {
         sobrepeso.style.backgroundColor = "white"
         obesidade.style.backgroundColor = "white"
         obesidadeGrave.style.backgroundColor = "#bbd8ff77"
-        resimc.innerHTML = imc
+        resImc.innerHTML = imc
     }
 }
 
 function limpar() {
-    var altura = document.getElementById("altura")
-    var peso = document.getElementById("peso")
-    var resimc = document.getElementById("res-imc")
 
     altura.value = ''
     peso.value = ''
@@ -73,5 +70,5 @@ function limpar() {
         obesidadeGrave.style.backgroundColor = "white"
     }
     
-    resimc.innerHTML = ''
+    resImc.innerHTML = ''
 }
